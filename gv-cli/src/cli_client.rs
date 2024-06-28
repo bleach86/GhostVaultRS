1156b168e1ccff20b3e3345f984e923c099fa05e
bleach86 <14421739+bleach86@users.noreply.github.com>
2024-06-28 04:02:19 -0500

    display version with help

--- gv-cli/src/cli_client.rs
+++ gv-cli/src/cli_client.rs
@@ -1,9 +1,3 @@
-// Copyright 2018 Google LLC
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 use serde::ser::StdError;
 use service::{
     config,
@@ -417,6 +411,7 @@ fn display_version() {
 }
 
 fn display_help() {
+    display_version();
     println!("Usage: gv-cli [OPTIONS] [METHOD] [ARGS]");
     println!("\nOptions:");
     println!("  --gv-data-dir=GV_DATA_DIR    Set the GhostVault data directory");
